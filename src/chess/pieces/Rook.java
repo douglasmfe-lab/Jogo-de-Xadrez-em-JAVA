159400e14e0b96037bad7bbcdf9a774dda91f087
Douglas Ferreira <douglasmfe@gmail.com>
2021-01-29 09:33:59 -0300

    Special move - Castling

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -12,11 +12,6 @@ public class Rook extends ChessPiece {
 	}
 
 	@Override
-	public String toString() {
-		return "R";
-	}
-
-	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
@@ -69,4 +64,8 @@ public class Rook extends ChessPiece {
 		return mat;
 	}
 
+	@Override
+	public String toString() {
+		return "R";
+	}
 }
